a835ba85750cf364d3f4c9ba0c793b3d0d2f2775
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-09-08 10:17:46 -0700

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace IndianStateCensusAnalyser
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Indian State Census Analyser ");
+            Console.ReadLine();
+        }
+    }
+}
